953552ce1cfbf9c46e8122da702c3b36bdb579d6
Paul Martin <paul.lakerfan@gmail.com>
2012-07-01 22:29:40 -0700

    Actual initial commit

---
+++ GUI Components/src/org/sandbox/MenuTest.java
@@ -0,0 +1,17 @@
+package org.sandbox;
+
+import javax.swing.JFrame;
+
+public class MenuTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MenuFrame menuFrame = new MenuFrame();
+		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		menuFrame.setSize(500, 200);
+		menuFrame.setVisible(true);
+	}
+
+}
---
+++ GUI Components/src/org/sandbox/SliderDemo.java
@@ -0,0 +1,18 @@
+package org.sandbox;
+
+import javax.swing.JFrame;
+
+public class SliderDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SliderFrame sliderFrame = new SliderFrame();
+		sliderFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sliderFrame.setSize(220, 270);
+		sliderFrame.setVisible(true);
+
+	}
+
+}
